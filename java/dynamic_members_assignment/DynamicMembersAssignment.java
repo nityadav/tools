5a8c280d0f2ac80c4f9e32ae39c0802b790e27e4
Nitin Yadav <ynitin@yahoo-inc.com>
2016-01-24 19:32:00 -0800

    adding dynamic_members_allocation

---
+++ java/dynamic_members_assignment/DynamicMembersAssignment.java
@@ -0,0 +1,39 @@
+import java.lang.reflect.*;
+import java.util.*;
+
+public class DynamicMembersAssignment{
+
+    private static class Template {
+                public int count;
+                        public String name;
+                                public Template(HashMap<String,String> map) throws NoSuchFieldException, IllegalAccessException
+                                {
+                                                Field[] members = getClass().getFields();
+                                                for (Field f: members) {
+                                                                    String fname = f.getName();
+                                                                                    String ftype = f.getType().toString();
+                                                                                    if (ftype.equals("int")) {
+                                                                                                            int fval = Integer.parseInt(map.get(fname));
+                                                                                                                                f.set(this, fval);
+
+                                                                                    }
+                                                                                                    else
+                                                                                                                            f.set(this, map.get(fname));
+
+                                                }
+
+                                }
+
+    }
+
+         public static void main(String []args) throws NoSuchFieldException, IllegalAccessException
+         {
+                      HashMap<String,String> map = new HashMap<String,String>();
+                               map.put("count","5450");
+                                        map.put("name","Nitin");
+                                                 Template obj = new Template(map);
+                                                          System.out.println(obj.count);
+
+         }
+
+}
---
+++ java/dynamic_members_assignment/README.md
@@ -0,0 +1,2 @@
+# Dynamic Members Allocation
+This is an example program for dynamically assigning members of a class. More examples can be found [here](http://examples.javacodegeeks.com/core-java/reflection/java-reflection-example).
