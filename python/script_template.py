bab9f98df1409b60ac395a9b23e725ef6bfba0fb
Nitin <nityadav@users.noreply.github.com>
2017-05-11 12:31:40 -0700

    one more use case added

--- python/script_template.py
+++ python/script_template.py
@@ -6,8 +6,10 @@ parser = argparse.ArgumentParser(description='Description of the script.')
 parser.add_argument('foo', help='Description of the required argument')
 parser.add_argument('-b', '--bar', dest='bar', help='Description of the optional argument')
 parser.add_argument('-f', dest='f', action='store_true', help='Description of the optional argument without a value')
+parser.add_argument('-d', dest='d', default='default_value', help='Description of the optional argument which has a default')
 args = parser.parse_args()
 
 print args.foo
 print args.bar
 print args.f
+print args.d
