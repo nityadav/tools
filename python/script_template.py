ad56578d4e47cdba9214d5ce646f86005be536d2
Nitin <nityadav@users.noreply.github.com>
2017-10-14 12:51:10 +0530

    update with main check

--- python/script_template.py
+++ python/script_template.py
@@ -2,14 +2,15 @@
 
 import argparse
 
-parser = argparse.ArgumentParser(description='Description of the script.')
-parser.add_argument('foo', help='Description of the required argument')
-parser.add_argument('-b', '--bar', dest='bar', help='Description of the optional argument')
-parser.add_argument('-f', dest='f', action='store_true', help='Description of the optional argument without a value')
-parser.add_argument('-d', dest='d', default='default_value', help='Description of the optional argument which has a default')
-args = parser.parse_args()
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='Description of the script.')
+  parser.add_argument('foo', help='Description of the required argument')
+  parser.add_argument('-b', '--bar', dest='bar', help='Description of the optional argument')
+  parser.add_argument('-f', dest='f', action='store_true', help='Description of the optional argument without a value')
+  parser.add_argument('-d', dest='d', default='default_value', help='Description of the optional argument which has a default')
+  args = parser.parse_args()
 
-print args.foo
-print args.bar
-print args.f
-print args.d
+  print args.foo
+  print args.bar
+  print args.f
+  print args.d
