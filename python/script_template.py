22e715717587cae9aa1dd139f867c936c5385adc
Nitin <nityadav@users.noreply.github.com>
2017-11-22 14:20:42 -0800

    Adding choices type of argument

--- python/script_template.py
+++ python/script_template.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 import argparse
 
 if __name__ == '__main__':
@@ -8,9 +6,11 @@ if __name__ == '__main__':
   parser.add_argument('-b', '--bar', dest='bar', help='Description of the optional argument')
   parser.add_argument('-f', dest='f', action='store_true', help='Description of the optional argument without a value')
   parser.add_argument('-d', dest='d', default='default_value', help='Description of the optional argument which has a default')
+  parser.add_argument('-c', dest='c', default='alice', choices=['alice, 'bob'], help='Description of the optional argument which has choices')
   args = parser.parse_args()
 
   print args.foo
   print args.bar
   print args.f
   print args.d
+  print args.c
